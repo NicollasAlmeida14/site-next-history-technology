1a5345ba82926b46adc83dc39e9dcee8071f7b65
NicollasAlmeida14 <nicollasalmeida1908@gmail.com>
2026-01-04 19:57:44 -0300

    Finalização da lógica do simulador financeiro e criação da pasta 'utils' para guardar um função de formatação de moedas

--- src/pages/FinancialSimulator/index.jsx
+++ src/pages/FinancialSimulator/index.jsx
@@ -1,78 +1,266 @@
-import DefaultHeader from "../../components/Header"
 import {
     Button,
     CardDescription,
+    LossSpan,
     Container,
     Description,
     DescriptionsContainer,
     InfosContainer,
     InfosWrapper,
+    InfoTd,
     Input,
     InputsContainer,
     InputsContent,
+    LossCardContainer,
+    LossCardContent,
     Main,
+    Mask,
     Option,
+    PrejudiceContainer,
+    ResultContainer,
     Select,
-    SpanInput
+    Span,
+    SpanInput,
+    StrikerInputsContent,
+    Subtitle,
+    SubtitleSpan,
+    Table,
+    TablesContainer,
+    Td,
+    Th,
+    Tr,
+    FineContainer,
+    TotalLossSpan,
+    LossReductionContainer,
+    LossReductionContent,
+    LossReductionWrapper,
+    LossReductionDescription,
+    TitleSpan,
+    ReductionSpan,
+    TotalReductionSpan
 } from "./styles"
 
+import DefaultHeader from "../../components/Header"
+
+import { FaArrowRight } from "react-icons/fa";
+
+import { useState } from "react"
+
+import formatCurrency from "../../utils/formatCurrency";
+
 function FinancialSimulator() {
+    const [selectedValue, setSelectedValue] = useState('#')
+
+    const [dailyBilling, setDailyBilling] = useState(0)
+
+    const [strikesDay, setStrikesDay] = useState(0)
+
+    const [quantityEmployee, setQuantityEmployee] = useState(0)
+
+    const [activateSummary, setActivateSummary] = useState(false)
+
+    const minFineBase = 443.97
+    const recidivismFine = 44396.84
+    const employeeFine = 104.31
+    const monthlyBilling = dailyBilling * 30
+
+    const loss = dailyBilling * strikesDay
+
+
+    const employeeLoss = quantityEmployee * employeeFine
+    const totalFine = employeeLoss + minFineBase + recidivismFine
+    const fineImpacts = ((totalFine / monthlyBilling) * 100).toFixed(2)
+    const totalLoss = loss + totalFine + employeeLoss
+    const totalSaved = totalFine
+    const maxFine = recidivismFine + employeeLoss
+
+    const validateInfos = () => {
+        if (selectedValue === '#' ||
+            (dailyBilling === '' || dailyBilling <= 0) ||
+            (quantityEmployee === '' || quantityEmployee <= 0) ||
+            (selectedValue === 'yes' && (strikesDay === '' || strikesDay <= 0))
+        ) {
+            alert('preencha as informações corretamente')
+        } else {
+            setActivateSummary(true)
+        }
+    }
+
     return (
-        <Container>
-            <DefaultHeader />
+        <Mask>
+            <Container>
+                <DefaultHeader />
 
-            <Main>
-                <DescriptionsContainer>
-                    <h1>Simulador Financeiro NH3</h1>
+                <Main>
+                    <DescriptionsContainer>
+                        <h1>Simulador Financeiro NH3</h1>
 
-                    <CardDescription>
-                        <Description>
-                            Um frigorífico deve ter sistemas de monitoramento de gás de acordo com a lei NR-15 e NR-36, e em caso de descumprimento, estará suscetível a multas e indenizações.
-                        </Description>
-                    </CardDescription>
+                        <CardDescription>
+                            <Description>
+                                Um frigorífico deve ter sistemas de monitoramento de gás de acordo com a lei NR-15 e NR-36, e em caso de descumprimento, estará suscetível a multas e indenizações.
+                            </Description>
+                        </CardDescription>
 
-                    <CardDescription>
-                        <Description>
-                            Calcule o prejuízo potencial em caso de vazamento de gás amônia no seu frigorífico e a recuperação com redução de riscos.
-                        </Description>
-                    </CardDescription>
-                </DescriptionsContainer>
+                        <CardDescription>
+                            <Description>
+                                Calcule o prejuízo potencial em caso de vazamento de gás amônia no seu frigorífico e a recuperação com redução de riscos.
+                            </Description>
+                        </CardDescription>
+                    </DescriptionsContainer>
 
-                <InfosContainer>
-                    <InfosWrapper>
-                        <InputsContainer>
-                            <InputsContent>
-                                <SpanInput>Informe seu faturamento diário</SpanInput>
-                                <Input />
-                            </InputsContent>
+                    <InfosContainer>
+                        <InfosWrapper>
+                            <InputsContainer>
+                                <InputsContent>
+                                    <SpanInput>Faturamento diário</SpanInput>
+                                    <Input
+                                        type="number"
+                                        onChange={(e) => Number(setDailyBilling(e.target.value))}
+                                    />
+                                </InputsContent>
 
-                            <InputsContent>
-                                <SpanInput>Quantidade de funcionários</SpanInput>
-                                <Input />
-                            </InputsContent>
-                        </InputsContainer>
+                                <InputsContent>
+                                    <SpanInput>Quantidade de funcionários</SpanInput>
 
-                        <InputsContainer>
-                            <InputsContent>
-                                <SpanInput>Houve paralisação?</SpanInput>
-                                <Select>
-                                    <Option>Escolha uma opção</Option>
-                                    <Option>Não</Option>
-                                    <Option>Sim</Option>
-                                </Select>
-                            </InputsContent>
+                                    <Input
+                                        type="number"
+                                        onChange={(e) => Number(setQuantityEmployee(e.target.value))}
+                                    />
+                                </InputsContent>
+                            </InputsContainer>
 
-                            {/* <InputsContent>
-                                <SpanInput>Quantos dias durou a paralisação?</SpanInput>
-                                <Input />
-                            </InputsContent> */}
-                        </InputsContainer>
+                            <InputsContainer>
+                                <InputsContent>
+                                    <SpanInput>Houve paralisação?</SpanInput>
+                                    <Select
+                                        onChange={(e) => setSelectedValue(e.target.value)}
+                                        defaultValue={'#'}
+                                    >
+                                        <Option value={'#'} disabled>Escolha uma opção</Option>
+                                        <Option value={'no'}>Não</Option>
+                                        <Option value={'yes'}>Sim</Option>
+                                    </Select>
+                                </InputsContent>
 
-                        <Button>Calcular</Button>
-                    </InfosWrapper>
-                </InfosContainer>
-            </Main>
-        </Container>
+                                <StrikerInputsContent theme={selectedValue === 'yes' ? 'valid' : ''}>
+                                    <SpanInput>Tempo de paralisação (dias)</SpanInput>
+                                    <Input type="number"
+                                        onChange={(e) => Number(setStrikesDay(e.target.value))}
+                                    />
+                                </StrikerInputsContent>
+                            </InputsContainer>
+
+                            <Button onClick={validateInfos}>Calcular</Button>
+                        </InfosWrapper>
+                    </InfosContainer>
+                </Main>
+
+                <ResultContainer theme={activateSummary === true ? 'visible' : ''}>
+                    <TablesContainer>
+                        <Table>
+                            <Tr>
+                                <Th colSpan={2}>Faturamento Mensal</Th>
+                            </Tr>
+
+                            <Tr>
+                                <InfoTd>Faturamento estimado</InfoTd>
+                                <Td>{formatCurrency(monthlyBilling)}</Td>
+                            </Tr>
+
+                            <Tr>
+                                <InfoTd>Impacto das multas</InfoTd>
+                                <Td>{`${formatCurrency(fineImpacts)}%`}</Td>
+                            </Tr>
+                        </Table>
+
+                        <Table>
+                            <Tr>
+                                <Th colSpan={2}>Multas</Th>
+                            </Tr>
+
+                            <Tr>
+                                <InfoTd>Multa mínima</InfoTd>
+                                <Td>{formatCurrency(minFineBase)}</Td>
+                            </Tr>
+
+                            <Tr>
+                                <InfoTd>Multa máxima em caso de reincidência</InfoTd>
+                                <Td>{formatCurrency(maxFine)}</Td>
+                            </Tr>
+
+                            <Tr>
+                                <InfoTd>Multa por funcionário</InfoTd>
+                                <Td>{formatCurrency(employeeFine)}</Td>
+                            </Tr>
+                        </Table>
+                    </TablesContainer>
+
+                    <PrejudiceContainer>
+                        <Subtitle>Prejuízo estimado</Subtitle>
+
+                        <LossCardContainer>
+                            <LossCardContent>
+                                <SubtitleSpan>Perda direta de faturamento:</SubtitleSpan>
+                                <LossSpan>
+                                    {selectedValue === 'yes' ? formatCurrency(loss) : formatCurrency(0)}
+                                </LossSpan>
+                            </LossCardContent>
+
+                            <LossCardContent>
+                                <SubtitleSpan>Multa pelo total de funcionários:</SubtitleSpan>
+                                <LossSpan>{formatCurrency(employeeLoss)}</LossSpan>
+                            </LossCardContent>
+                        </LossCardContainer>
+
+                        <FineContainer>
+                            <Span>
+                                Em caso de reincidência
+                                <i> <FaArrowRight /> </i>
+                            </Span>
+
+                            <div>
+                                <SubtitleSpan>Total de multas:</SubtitleSpan>
+                                <LossSpan>{formatCurrency(totalFine)}</LossSpan>
+                            </div>
+                        </FineContainer>
+
+                        <TitleSpan>Perda total estimada:</TitleSpan>
+                        <TotalLossSpan>{formatCurrency(totalLoss)}</TotalLossSpan>
+                    </PrejudiceContainer>
+
+                    <LossReductionContainer>
+                        <Subtitle>Com nosso sistema...</Subtitle>
+
+                        <LossReductionContent>
+                            <LossReductionWrapper>
+                                <SubtitleSpan>Simulação de redução de prejuízo</SubtitleSpan>
+
+                                <LossReductionDescription>
+                                    Com nossa equipe, você terá uma melhoria de
+                                    <br />
+                                    <br />
+                                    <ReductionSpan>{`${formatCurrency(fineImpacts)}%`}</ReductionSpan>
+                                    <br />
+                                    <br />
+                                    em cima do seu faturamento mensal referente ao total de multas
+                                </LossReductionDescription>
+                            </LossReductionWrapper>
+
+                            <LossReductionWrapper>
+                                <SubtitleSpan>Eliminando a perda de:</SubtitleSpan>
+
+                                <ReductionSpan>{formatCurrency(totalFine)}</ReductionSpan>
+
+                                <LossReductionDescription>em multas!</LossReductionDescription>
+                            </LossReductionWrapper>
+                        </LossReductionContent>
+
+                        <TitleSpan>Total Poupado:</TitleSpan>
+                        <TotalReductionSpan>{formatCurrency(totalSaved)}</TotalReductionSpan>
+                    </LossReductionContainer>
+                </ResultContainer>
+            </Container>
+        </Mask>
     )
 }
 
--- src/pages/FinancialSimulator/styles.js
+++ src/pages/FinancialSimulator/styles.js
@@ -1,4 +1,33 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const arrowEffect = keyframes`
+    0%, 100% {
+        transform: translateX(10px);
+    }
+
+    50% {
+        transform: translateX(0);
+    }
+`
+
+const textEffect = keyframes`
+    0%, 100% {
+        scale: 1.5;
+    }
+
+    50% {
+        scale: 1;
+    }
+`
+
+export const Mask = styled.div`
+    /* width: 100%;
+    height: 100%;
+    background: linear-gradient(109deg, rgba(10, 12, 16, 0.99) 15%, rgba(10, 12, 16, 0.7) 50%, rgba(10, 12, 16, 0.99) 85%);
+    position: fixed;
+    top: 0;
+    z-index: 1000; */
+`
 
 export const Container = styled.div`
     padding-top: 9rem;
@@ -63,6 +92,12 @@ export const InputsContent = styled.div`
     gap: 0.5rem;
 `
 
+export const StrikerInputsContent = styled.div`
+    display: ${props => props.theme === 'valid' ? 'flex' : 'none'};
+    flex-direction: column;
+    gap: 0.5rem;
+`
+
 export const SpanInput = styled.span`
     font-size: 15px;
     font-weight: bold;
@@ -111,4 +146,193 @@ export const Button = styled.button`
     &:active {
         opacity: 0.8;
     }
+`
+
+export const ResultContainer = styled.div`
+    position: fixed;
+    top: 25%;
+    left: 25%;
+    background-color: #ffffff;
+    height: 60vh;
+    width: 50vw;
+    display: ${props => props.theme === 'visible' ? 'flex' : 'none' };
+    flex-direction: column;
+    border-radius: 15px;
+    box-shadow: 0 4px 8px 3px rgba(0, 0, 0, 0.15);
+    padding: 2rem;
+    align-items: center;
+    overflow-y: auto;
+    gap: 2rem;
+`
+
+export const TablesContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    width: 100%;
+    gap: 1rem;
+    align-items: center;
+    padding-bottom: 2rem;
+    border-bottom: solid 2px #a038f2;
+`
+
+export const Table = styled.table`
+    border-collapse: collapse;
+    text-align: center;
+    vertical-align: middle;
+`
+
+export const Tr = styled.tr``
+
+export const Th = styled.th`
+    border: solid 1px #000000;
+    padding: 0.7rem 0;
+    background-color: #5A189A;
+    color: #ffffff;
+    font-size: 18px;
+`
+
+export const InfoTd = styled.td`
+    border: solid 1px #000000;
+    font-weight: bold;
+    background-color: #a038f2;
+    color: #ffffff;
+    height: 50px;
+    padding: 5px;
+`
+
+export const Td = styled.td`
+    border: solid 1px #000000;
+    width: 50%;
+    font-size: 18px;
+`
+
+export const PrejudiceContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+    width: 100%;
+    border-bottom: solid 2px #a038f2;
+    padding-bottom: 2rem;
+`
+
+export const Subtitle = styled.h2`
+    color: #a038f2;
+`
+
+export const LossCardContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    width: 100%;
+    gap: 2rem;
+    margin-top: 1rem;
+`
+
+export const LossCardContent = styled.div`
+    background-color: #ffffff;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    padding: 1rem;
+    align-items: center;
+    border-radius: 15px;
+    height: 150px;
+    box-shadow: 0 4px 8px 3px rgba(160, 56, 242, 0.4);
+    border: solid 2px #a038f2;
+`
+
+export const SubtitleSpan = styled.span`
+    color: #5A189A;
+    font-weight: bold;
+    font-size: 20px;
+    text-align: center;
+`
+
+export const LossSpan = styled.span`
+    font-size: 35px;
+    font-weight: bold;
+    color: #FF0000;
+    text-shadow: 2px 2px 2px rgba(255, 0, 0, 0.25);
+    text-align: center;
+`
+
+export const Span = styled.span`
+    font-size: 22px;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+
+    i {
+        animation: ${arrowEffect} infinite 2s ;
+    }
+`
+
+export const FineContainer = styled.div`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    justify-content: space-around;
+
+    div {
+        display: flex;
+        flex-direction: column;
+    }
+`
+
+export const TitleSpan = styled.div`
+    font-size: 32px;
+    font-weight: bold;
+    color: #5a189a;
+`
+
+export const TotalLossSpan = styled.span`
+    font-size: 45px;
+    font-weight: bold;
+    color: #FF0000;
+    text-shadow: 2px 2px 2px rgba(255, 0, 0, 0.25);
+    text-align: center;
+    animation: ${textEffect} infinite 2s ease-in-out;
+`
+
+export const LossReductionContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+`
+
+export const LossReductionContent = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    align-items: center;
+    height: 200px;
+`
+
+export const LossReductionWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    align-items: center;
+    text-align: center;
+    justify-content: space-around;
+    height: 100%;
+`
+
+export const LossReductionDescription = styled.p`
+    font-size: 18px;
+`
+
+export const ReductionSpan = styled.span`
+    font-size: 35px;
+    font-weight: bold;
+    color: #00FF9C;
+    text-shadow: 2px 2px 2px rgba(0, 255, 156, 0.5);
+`
+
+export const TotalReductionSpan = styled.span`
+    font-size: 45px;
+    font-weight: bold;
+    color: #00FF9C;
+    text-shadow: 2px 2px 2px rgba(0, 255, 156, 0.5);
+    animation: ${textEffect} infinite 2s ease-in-out;
 `
