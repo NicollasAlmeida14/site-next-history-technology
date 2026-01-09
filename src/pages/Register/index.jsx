09410ece32f05e66b22e44ddde9da01a80943c34
NicollasAlmeida14 <nicollasalmeida1908@gmail.com>
2026-01-08 21:16:44 -0300

    Finalização da estilização da tela de cadastro

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -7,84 +7,91 @@ import {
   InfosContainer,
   Input,
   InputsContainer,
-  InputSpan,
+  InputLabel,
   InputWrapper,
   Link,
   RegisterSpan,
   Title
 } from "./styles";
 
+import Logo from '../../assets/logo-nh3.png'
+
 import { FaArrowLeft } from "react-icons/fa";
 
+import { useState } from "react";
+
 function Register() {
+  const [userEmail, setUserEmail] = useState('')
+  const [userName, setUserName] = useState('')
+  const [userPhone, setUserPhone] = useState('')
+  const [userId, setUserId] = useState('')
+  const [userPassword, setUserPassword] = useState('')
+  const [repeatPassword, setRepeatPassword] = useState('')
+  const [admissionCode, setAdmissionCode] = useState('')
+
   return (
     <Container>
       <BannerContainer>
-        <Link>
+        <Link href="/">
           <FaArrowLeft />
           Voltar
         </Link>
 
-        <BannerSpan>Bem-vindo à tela de cadastro</BannerSpan>
+        <BannerSpan>Bem-vindo à <br /> tela de cadastro</BannerSpan>
       </BannerContainer>
 
       <InfosContainer>
         <Title>Cadastre-se</Title>
 
+        <img src={Logo} alt="" />
+
         <InputsContainer>
           <DuoContainer>
             <InputWrapper>
-              <InputSpan>Email:</InputSpan>
-
-              <Input />
+              <Input placeholder="" id="user-email" />
+              <InputLabel htmlFor="user-email">Email</InputLabel>
             </InputWrapper>
 
             <InputWrapper>
-              <InputSpan>Nome:</InputSpan>
-
-              <Input />
+              <Input placeholder="" id="user-name" />
+              <InputLabel htmlFor="user-name">Nome</InputLabel>
             </InputWrapper>
           </DuoContainer>
 
           <DuoContainer>
             <InputWrapper>
-              <InputSpan>Telefone:</InputSpan>
-
-              <Input />
+              <Input placeholder="" id="user-phone" />
+              <InputLabel htmlFor="user-phone">Telefone</InputLabel>
             </InputWrapper>
 
             <InputWrapper>
-              <InputSpan>CPF:</InputSpan>
-
-              <Input />
+              <Input placeholder="" id="user-id" />
+              <InputLabel htmlFor="user-id">CPF</InputLabel>
             </InputWrapper>
           </DuoContainer>
 
           <DuoContainer>
             <InputWrapper>
-              <InputSpan>Senha:</InputSpan>
-
-              <Input />
+              <Input placeholder="" id="user-password" />
+              <InputLabel htmlFor="user-password">Senha</InputLabel>
             </InputWrapper>
 
             <InputWrapper>
-              <InputSpan>Confirmar senha:</InputSpan>
-
-              <Input />
+              <Input placeholder="" id="repeat-password" />
+              <InputLabel htmlFor="repeat-password">Confirmar senha</InputLabel>
             </InputWrapper>
           </DuoContainer>
 
           <InputWrapper>
-            <InputSpan>Código de ativação:</InputSpan>
-
-            <Input />
+            <Input placeholder="" id="admission-code" />
+            <InputLabel htmlFor="admission-code">Código de ativação</InputLabel>
           </InputWrapper>
         </InputsContainer>
 
         <Button>Cadastrar</Button>
 
         <RegisterSpan>
-          Já é cadastrado?
+          Já é cadastrado? 
           <a href="/tela-de-login-nh3">Acesse sua conta</a>
         </RegisterSpan>
       </InfosContainer>
--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -1,27 +1,191 @@
 import styled from "styled-components";
 
-export const Container = styled.div``
+import Banner from '../../assets/banner.png'
 
-export const BannerContainer = styled.div``
+export const Container = styled.div`
+    height: 100vh;
+    display: grid;
+    grid-template-columns: 35vw 65vw;
+    align-items: center;
+`
 
-export const Link = styled.a``
+export const BannerContainer = styled.div`
+    background-image: url(${Banner});
+    background-size: cover;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    align-items: center;
+    padding: 0.5rem;
+`
 
-export const BannerSpan = styled.span``
+export const Link = styled.a`
+    position: relative;
+    margin: 1.5rem 24rem 0 0;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    font-size: 20px;
+    font-weight: bold;
+    color: #ffffff;
+    text-decoration: none;
+    padding: 0.5rem;
+    width: fit-content;
+    z-index: 0;
 
-export const InfosContainer = styled.div``
+    &::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        height: 100%;
+        z-index: -1;
+        background-color: #ffffff;
+        width: 0;
+        border-radius: 15px;
+        transition: width ease-in-out 0.5s;
+    }
 
-export const Title = styled.h1``
+    &:hover::after {
+        width: 100%;
+    }
 
-export const InputsContainer = styled.div``
+    &:hover {
+        color: #a038f2;
+        transition: ease-in-out 0.5s;
+    }
 
-export const DuoContainer = styled.div``
+    &:active {
+        opacity: 0.5;
+        transition: ease-in 0.2s;
+    }
+`
 
-export const InputWrapper = styled.div``
+export const BannerSpan = styled.span`
+    text-align: center;
+    margin: auto 0;
+    border-bottom: solid 5px #ffffff;
+    border-top: solid 5px #ffffff;
+    padding: 1rem 0;
+    width: max-content;
+    color: #ffffff;
+    font-weight: bold;
+    font-size: 50px;
+`
 
-export const InputSpan = styled.span``
+export const InfosContainer = styled.div`
+    background-color: #ebebeb;
+    height: 100%;
+    position: relative;
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
 
-export const Input = styled.input``
+    img {
+        width: 180px;
+        position: absolute;
+        top: 0;
+        right: 0;
+    }
+`
 
-export const Button = styled.button``
+export const Title = styled.h1`
+    font-size: 50px;
+    color: #5630D8;
+    border-bottom: solid 5px #5630D8;
+    padding-bottom: 0.7rem;
+`
 
-export const RegisterSpan = styled.span``
+export const InputsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1.7rem;
+    align-items: center;
+`
+
+export const DuoContainer = styled.div`
+    display: flex;
+    gap: 2.5rem;
+`
+
+export const InputWrapper = styled.div`
+    position: relative;
+`
+
+export const InputLabel = styled.label`
+    font-size: 1.3rem;
+    position: absolute;
+    top: 15px;
+    left: 5px;
+    transition: all .5s;
+    pointer-events: none;
+    color: #5630D8;
+`
+
+export const Input = styled.input`
+    border: none;
+    background: none;
+    outline: none;
+    width: 250px;
+    border-bottom: solid 2px rgba(86, 48, 216, 0.25);
+    opacity: 100%;
+    padding: 0.7rem 0.5rem;
+    font-size: 1.1rem;
+
+    &:focus ~ ${InputLabel},
+    &:not(:placeholder-shown) ~ ${InputLabel}{
+        top: -10px;
+        left: 0;
+        font-size: 1rem;
+        color: #5a189a; 
+        font-weight: 500;
+    }
+
+    &:focus ,
+    &:not(:placeholder-shown) {
+        border-bottom: solid 2px #5a189a;
+    }
+`
+
+export const Button = styled.button`
+    border: none;
+    border-radius: 15px;
+    padding: 0.7rem 0.35rem;
+    background-color: #5630D8;
+    font-size: 1.5rem;
+    width: 220px;
+    color: #ffffff;
+    box-shadow: 2px 2px 1px 0 #06004F;
+    cursor: pointer;
+    font-weight: bold;
+    transition: all ease-in-out 0.3s;
+
+    &:hover {
+        transform: translateY(-5px);
+    }
+
+    &:active {
+        transform: translateY(0);
+        opacity: 0.7;
+    }
+`
+
+export const RegisterSpan = styled.span`
+    font-size: 1.2rem;
+    color: #5630D8;
+    font-weight: bold;
+
+    a {
+        opacity: 0.5;
+        transition: all ease-in-out 0.3s;
+
+        &:hover {
+            text-decoration: none;
+            color: #5630D8;
+            opacity: 0.8;
+        }
+    }
+`
